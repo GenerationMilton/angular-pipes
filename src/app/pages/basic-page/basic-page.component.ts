d036a801e65b6151596e0dc012d03b0cef678f96
Milton Munoz <livemilton@gmail.com>
2025-06-11 21:45:46 -0500

    angular :sparkles: Date pipe

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,9 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [ LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [ LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
  
 })
@@ -11,6 +11,20 @@ export default class BasicPageComponent {
 
   nameLower = signal('milton');
   nameUpper = signal('MILTON');
-  fullName= signal('MiLToN MuNOz')
- 
+  fullName = signal('MiLToN MuNOz');
+
+
+  customDate = signal( new Date() );
+
+  tickingDateEffect = effect((onCleanup)=>{
+    const interval = setInterval(()=>{
+      this.customDate.set(new Date());
+      console.log('tick');
+    }, 1000);
+
+    onCleanup(()=>{
+      clearInterval(interval);
+    })
+
+  })
 }
