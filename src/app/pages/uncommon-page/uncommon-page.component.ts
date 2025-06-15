4cc5f5ebda68297cd3536868b72ebb247185e1fc
Milton Munoz <livemilton@gmail.com>
2025-06-15 11:17:08 -0500

    angular :sparkles: I18nSelectPipe internationalization Pipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,45 @@
-import {  Component } from '@angular/core';
+import {  Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
+import { I18nSelectPipe } from '@angular/common';
+
+//2 clients
+const client1 ={
+  name: 'Milton',
+  gender: 'male',
+  age: 39,
+  address: 'Bogota, Colombia'
+};
+
+const client2 ={
+  name: 'Fabiana',
+  gender: 'female',
+  age: 35,
+  address: 'Cali, Colombia'
+};
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent,I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+
+  //i18m Select
+
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  //toggle to change client
+  changeClient(){
+    if(this.client() === client1){
+      this.client.set(client2);
+      return;
+    }
+    this.client.set(client1);
+
+  }
+ }
