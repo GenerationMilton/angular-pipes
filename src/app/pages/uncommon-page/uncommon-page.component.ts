c34cf4d61ad7b27e667ce97db7feb8aff892b553
Milton Munoz <livemilton@gmail.com>
2025-06-15 12:18:52 -0500

    angular :sparkles: AsyncPipe in pipes

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,30 +1,47 @@
-import {  Component, signal } from '@angular/core';
-import { CardComponent } from "../../components/card/card.component";
-import { I18nPluralPipe, I18nSelectPipe, JsonPipe, SlicePipe, UpperCasePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import {
+  AsyncPipe,
+  I18nPluralPipe,
+  I18nSelectPipe,
+  JsonPipe,
+  KeyValuePipe,
+  SlicePipe,
+  TitleCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
 
 //2 clients
-const client1 ={
+const client1 = {
   name: 'Milton',
   gender: 'male',
   age: 39,
-  address: 'Bogota, Colombia'
+  address: 'Bogota, Colombia',
 };
 
-const client2 ={
+const client2 = {
   name: 'Fabiana',
   gender: 'female',
   age: 35,
-  address: 'Cali, Colombia'
+  address: 'Cali, Colombia',
 };
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe, SlicePipe, JsonPipe, UpperCasePipe],
+  imports: [
+    CardComponent,
+    I18nSelectPipe,
+    I18nPluralPipe,
+    SlicePipe,
+    JsonPipe,
+    UpperCasePipe,
+    KeyValuePipe,
+    TitleCasePipe,
+    AsyncPipe
+  ],
   templateUrl: './uncommon-page.component.html',
 })
 export default class UncommonPageComponent {
-
-
   //i18m Select
 
   client = signal(client1);
@@ -35,23 +52,21 @@ export default class UncommonPageComponent {
   };
 
   //toggle to change client
-  changeClient(){
-    if(this.client() === client1){
+  changeClient() {
+    if (this.client() === client1) {
       this.client.set(client2);
       return;
     }
     this.client.set(client1);
-
   }
 
   // i18n Plural
   clientsMap = signal({
-    '=0':'no tenemos ningún cliente esperando',
+    '=0': 'no tenemos ningún cliente esperando',
     '=1': 'tenemos un cliente esperando',
-    '=2':'tenemos 2 clientes esperando',
+    '=2': 'tenemos 2 clientes esperando',
     other: 'tenemos # clientes esperando',
-  })
-
+  });
 
   clients = signal([
     'Maria',
@@ -64,8 +79,26 @@ export default class UncommonPageComponent {
     'Carlos',
   ]);
 
-    deleteClient() {
-      this.clients.update( prev => prev.slice(1) )
-    }
-    
- }
+  deleteClient() {
+    this.clients.update((prev) => prev.slice(1));
+  }
+
+  //keyValue Pipe
+  profile = {
+    name: 'Milton',
+    age: 36,
+    addres: 'Ottawa, Canada',
+  }
+
+  // Async Pipe
+  promiseValue: Promise<string> = new Promise((resolve, reject)=> {
+
+    setTimeout(()=>{
+      //reject('Tenemos un error en la data');
+      resolve('Tenemos data en la promesa.')
+      console.log('Promesa finalizada');
+
+    }, 3500);
+
+  })
+}
