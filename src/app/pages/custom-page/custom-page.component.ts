be288232284e01464148e1ceb6558cdb9097bd4a
Milton Munoz <livemilton@gmail.com>
2025-06-18 21:36:22 -0500

    angular :sparkles: personalized pipe - HeroTextColor

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -3,10 +3,13 @@ import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/hero.data';
 import { canFlyPipe } from "../../pipes/canfly.pipe";
 import { heroColorPipe } from '../../pipes/hero-color.pipe';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { TitleCasePipe } from '@angular/common';
+import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, canFlyPipe, heroColorPipe],
+  imports: [ToggleCasePipe, canFlyPipe, heroColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
